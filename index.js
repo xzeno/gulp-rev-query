78087904fb232e750557c32997f252b734f4f6bb
xzeno <34212539+xzeno@users.noreply.github.com>
2019-01-07 00:37:57 +0300

    update

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ module.exports = function(ver) {
     // convert a-xxxxxxxx.css to a.css?ver=xxxxxxxx
     function hashToQuery(file) {
         var content = new String(file.contents);
-        console.log(content);
         content = content.replace(/(\-(\w+))([\.\w]+)/g, function($, $1, $2, $3) {
             return $3 + '?' + ver + '=' + $2;
-        }).replace(/build/g, '/build');
-        console.log(content);
+        })
+        .replace(/\n[\s]+\"/g, '\n  "/');
         file.contents = new Buffer(content);
         file.ver = ver;
         return file;
