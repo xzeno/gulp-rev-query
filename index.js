4c08ae6747b84e586d69046cf4d7c98c628fd6a8
xzeno <34212539+xzeno@users.noreply.github.com>
2019-01-07 00:14:48 +0300

    regex issue, add build folder

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ module.exports = function(ver) {
     // convert a-xxxxxxxx.css to a.css?ver=xxxxxxxx
     function hashToQuery(file) {
         var content = new String(file.contents);
-        content = content.replace(/(\-(\w+))(\.\w+)/g, function($, $1, $2, $3) {
+        console.log(content);
+        content = content.replace(/(\-(\w+))([\.\w]+)/g, function($, $1, $2, $3) {
             return $3 + '?' + ver + '=' + $2;
-        })
+        }).replace(/build/g, '/build');
+        console.log(content);
         file.contents = new Buffer(content);
         file.ver = ver;
         return file;
